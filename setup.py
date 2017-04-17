ae34b29bbef1709dd842d4679c03c0ac602a2c8f
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-04-17 17:44:54 -0400

    Basic setup.py from python-packaging.readthedocs.io/en/latest/minimal.html

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='corner_hist',
+      version='0.1',
+      description='Corner plot for given histograms',
+      author='Thorben Menne',
+      author_email='',
+      license='MIT',
+      packages=['corner_hist'],
+      zip_safe=False)
